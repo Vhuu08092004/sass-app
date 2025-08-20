5ba3169714818ab94ccee453ad2ea9fb0937765b
quachhuuvinh <vhuu3208@gmail.com>
2025-08-20 20:27:39 +0700

    page companions add

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,51 @@
 import React from 'react'
 import {Button} from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import {recentSessions} from "@/constants";
 
 const Page = () => {
-  return (
-    <div>
-        <h1 className="text-2xl underline">Welcome to my saas app</h1>
-        <Button>
-            Click Me
-        </Button>
-    </div>
-  )
+    return (
+        <main>
+            <h1>Popular Companions</h1>
+            <section className="home-section">
+                <CompanionCard
+                    id="123"
+                    name="Neura the Brainy Explorer"
+                    topic="Neural Network of the Brain"
+                    subject="science"
+                    duration={45}
+                    color="#ffda6e"
+                />
+                <CompanionCard
+                    id="456"
+                    name="Verba the Vocabulary Builder"
+                    topic="English Literature "
+                    subject="Language"
+                    duration={30}
+                    color="#e5d0ff"
+                />
+                <CompanionCard
+                    id="789"
+                    name="Countsy the Number Wizard"
+                    topic="Derivatives & Integrals"
+                    subject="maths"
+                    duration={30}
+                    color="#dde7ff"
+                />
+            </section>
+
+            <section className="home-section">
+                <CompanionsList
+                    title="Recent Complete Sessions"
+                    companions={recentSessions}
+                    classNames="w-2/3 max-lg:w-full"
+                />
+                <CTA/>
+            </section>
+        </main>
+    )
 }
 
 export default Page
